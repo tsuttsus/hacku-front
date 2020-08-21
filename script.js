3dcfdf34842b0141ca1b77b54cdecb8088d6e12c
unknown <brvotsuttsu@gmail.com>
2020-08-21 14:13:49 +0900

    08211413

--- script.js
+++ script.js
@@ -14,12 +14,37 @@ input.addEventListener("change",function(){
  * 2.post/getの処理
  * resが返ってくるので、それを変数に代入
  */
-const submit=document.getElementById("submit");
 
+ let reponse = null;
+
+ //送信ボタンのDOMを取得
+const submit=document.getElementById("submit");
+//クリックされたら...
 submit.addEventListener("click",function(){
     console.log('ボタン');
+    var formdata = new FormData();
+    formdata.append("file",file);
+    //生成
+    var xmlhttp=new XMLHttpRequest();
+    //準備
+    xmlhttp.onreadystatechange = function () {
+        // 成功した場合
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          // alert(xmlhttp.responseText);
+          var response=xmlhttp.responseText;
+          showResponse(response)
+        }
+      };
+    // APIを開いて
+    xmlhttp.open("POST", "", true);
+    // 叩く。
+    xmlhttp.send(file);
+
 })
 
 /**
  * 3.resを表示
  */
+function showReponse(responseText){
+  console.log(responseText)
+}
